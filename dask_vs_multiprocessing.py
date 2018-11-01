9f7d9a1982069ff5403f8f941fdbd38d02326be1
Nathan Miles <nmiles@stsci.edu>
2018-11-01 17:10:43 -0400

    added function to run things serially so we can establish a baseline

--- dask_vs_multiprocessing.py
+++ dask_vs_multiprocessing.py
@@ -7,6 +7,9 @@ import time
 def combine(a=0, b=0, c=0):
     """ Simple function to compute the sum of three numbers
 
+    Emulate a more complicated task by telling the computer to wait for 0.5
+    seconds before computing and returning the sum of a, b, and c.
+
     Parameters
     ----------
     a
@@ -17,7 +20,41 @@ def combine(a=0, b=0, c=0):
     -------
     a + b + c
     """
+    time.sleep(0.5)
     return a + b + c
+
+
+def run_serial():
+    """ Run the computation serially to establish a baseline
+
+    Returns
+    -------
+    results : list of results containing the sums of the 3 input numbers
+    runtime : the time is took to complete the task
+    """
+    # Get the start time
+    st = time.time()
+
+    # Convenience function for generating a list of monotonically increasing
+    # numbers starting from start and ending at end.
+    f = lambda start, end: [i for i in range(start, end, 1)]
+
+    # Generate three separate lists of 100 numbers
+    a = f(1, 10)
+    b = f(10, 21)
+    c = f(20, 31)
+
+    # Combine the three lists to create a list of 100 tuples of three numbers
+    # i.e. [(a1, b1, c1), (a2, b2, c2),..., (a100, b100, c100)]
+    inputs = list(zip(a, b, c))
+    results= []
+    for arg in inputs:
+        results.append(combine(*arg))
+
+    # Get the end time and compute the total runtime
+    et = time.time()
+    runtime = et - st
+    return results, runtime
 
 
 def use_multiprocessing():
@@ -36,9 +73,9 @@ def use_multiprocessing():
     f = lambda start, end: [i for i in range(start, end, 1)]
 
     # Generate three separate lists of 100 numbers
-    a = f(1, 101)
-    b = f(101, 201)
-    c = f(301, 401)
+    a = f(1, 10)
+    b = f(10, 21)
+    c = f(20, 31)
 
     # Combine the three lists to create a list of 100 tuples of three numbers
     # i.e. [(a1, b1, c1), (a2, b2, c2),..., (a100, b100, c100)]
@@ -54,7 +91,7 @@ def use_multiprocessing():
     return results, runtime
 
 
-def use_dask():
+def use_dask(scheduler='processes'):
     """Parallelize the summing of 100 pairs of 3 numbers with dask
 
     Returns
@@ -70,9 +107,9 @@ def use_dask():
     f = lambda start, end: [i for i in range(start, end, 1)]
 
     # Generate three separate lists of 100 numbers
-    a = f(1, 101)
-    b = f(101, 201)
-    c = f(301, 401)
+    a = f(1, 10)
+    b = f(10, 21)
+    c = f(20, 31)
 
     # Combine the three lists to create a list of 100 tuples of three numbers
     # i.e. [(a1, b1, c1), (a2, b2, c2),..., (a100, b100, c100)]
@@ -85,7 +122,7 @@ def use_dask():
     # Tell dask to execute the delayed objects using 8 processes
     results = list(dask.compute(*results,
                                 num_workers=8,
-                                scheduler='processes'))
+                                scheduler=scheduler))
 
     # Get the time at the end and compute the total runtime
     et = time.time()
@@ -100,15 +137,20 @@ def compare():
     # Run trivial example using dask
     dask_results, dask_time = use_dask()
 
+    # Run trivial example serially
+    serial_results, serial_time = run_serial()
+
     # Check to see that each package returns the same results
     # If they do, compare the times
     try:
         assert dask_results == mp_results
+        assert serial_results == dask_results
     except AssertionError:
         print('Results are different!')
         print('This should never happen!')
     else:
         print('Time to compute:')
+        print('serially: {:.4f}'.format(serial_time))
         print('dask: {:.4f}s'.format(dask_time))
         print('multiprocessing: {:.4f}s'.format(mp_time))
 
